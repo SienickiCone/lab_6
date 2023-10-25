79d055ce084021f971984570d0745d29d4398161
mj 🇵🇭 <mjtady@gmail.com>
2023-10-25 10:05:41 -0400

    decode edits

--- passwordlab_6.py
+++ passwordlab_6.py
@@ -10,6 +10,17 @@ def encode(password):
     # Warning, if you are using this function, the value return is a string, not an integer.
 
 
+def decode(user_pass):
+    decode_list = []
+    for i in range(0, len(user_pass)):
+        if user_pass[i] == 0 or 1 or 2:
+            decode_list.append(str(int(user_pass[i]) + 10 - 3)[-1])
+        else:
+            decode_list.append(str(int(user_pass[i]) - 3)[-1])
+    new_pass = ''.join(decode_list)
+    return new_pass
+
+
 def menu():
     print('Menu')
     print('-' * 13)
@@ -30,7 +41,8 @@ def main():
             encode_password = encode(password)
             print("Your password has been encoded and stored!\n")
         elif user_selection == '2':
-            print(f'The encoded password is {encode_password}, and the original password is {decode(encode_password)}\n')
+            print(
+                f'The encoded password is {encode_password}, and the original password is {decode(encode_password)}\n')
         elif user_selection == '3':
             continue
 
